0dc9331231c20a0409a75c687ce886cb0bb37ca8
Andriy Barinov <andriybarinov@gmail.com>
2023-09-23 21:16:48 +0300

    add footer

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const swiper1 = new Swiper('.js--destination', {
+  slidesPerView: 3,
+  paginationClickable: true,
+  spaceBetween: 30,
+  navigation: {
+    nextEl: '.destination-btn--prev',
+    prevEl: '.destination-btn--next',
+  },
+});
