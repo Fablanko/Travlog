7c95cadd3a9650732d2fbdb2b4bc44d0f3699821
Andriy Barinov <andriybarinov@gmail.com>
2023-09-24 16:26:14 +0300

    add testimonials

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
 const swiper1 = new Swiper('.js--destination', {
   slidesPerView: 3,
-  paginationClickable: true,
   spaceBetween: 30,
   navigation: {
-    nextEl: '.destination-btn--prev',
-    prevEl: '.destination-btn--next',
+    nextEl: '.destination-control-next',
+    prevEl: '.destination-control-prev',
+  },
+});
+
+const swiper2 = new Swiper('.js--testimonials', {
+  slidesPerView: 1,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+  },
+  navigation: {
+    nextEl: '.testimonials-control-next',
+    prevEl: '.testimonials-control-prev',
   },
 });
--- timing.md
+++ timing.md
@@ -1,12 +1,11 @@
 Prpearing      45 min
 Header         15 min
-
 Primary        1 h 55 min
 Services       25 min
 Destination    2 h
-Point          11.00--
+Point          1 h 10 min (11.00--12.09)
 Features
-Testimonials
+Testimonials  12.42 --13.31 15.35--
 
 Subscribe      30 min
 Footer         45 min
